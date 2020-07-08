632b229601d8137f0f60145a0e1384f80fcfa448
stormsamurai1 <stormsamurai1@yahoo.com.br>
2020-07-08 14:33:32 -0300

    Criando componente de requisição para todos os verbos http

--- src/react/Components/LeftSideBar/Styles.js
+++ src/react/Components/LeftSideBar/Styles.js
@@ -8,11 +8,18 @@ export const LeftSideBar = Styled.div`
 
     align-items: center;
     justify-content: center;
+
+    border-radius: 8px;
+    box-shadow: 0px 4px 15px rgba(0, 0, 0, 0.08);
+    background: #555555;
 `;
 
 export const InputContainer = Styled.div`
     display: flex;
     flex-direction: column;
+
+
+    align-items: center;
 
     height: 300px;
     width: 200px;
@@ -20,8 +27,61 @@ export const InputContainer = Styled.div`
     border-radius: 8px;
     box-shadow: 0px 4px 15px rgba(0, 0, 0, 0.08);
 
+    background: #e6e6f0;
+`;
+
+export const DataTitle = Styled.a`
+    font-family: Roboto;
+    font-style: normal;
+    font-weight: 200;
+    font-size: 15px;
+
+    line-height: 21px;
+
+    color: #000000;
 `;
 
 export const DataInput = Styled.input`
-    margin: 8px;
+    margin: 4px;
+`;
+
+export const SendRequestButton = Styled.button`
+    background: #7a98F5;
+    align-items: center;
+    justify-content: center;
+    
+    border-radius: 8px;
+`;
+
+export const VerbsSelector = Styled.select`
+
+`;
+
+export const VerbsPicker = Styled.option`
+`;
+
+export const ResponseVisualizationContainer = Styled.div`
+    display: flex;
+
+    margin: 10px;
+    width: 70%;
+    height: 80%;
+
+    overflow: auto;
+
+    border-radius: 8px;
+    box-shadow: 0px 4px 15px rgba(0, 0, 0, 0.08);  
+`;
+
+export const ResponseVisualizationText = Styled.p`
+
+    font-family: Roboto;
+    font-style: normal;
+    font-weight: 200;
+    font-size: 15px;
+
+    line-height: 21px;
+
+    color: #000000;
+
 `;
